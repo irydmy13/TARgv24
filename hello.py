35070c48aa45dc6a8ed74705c06eedbcf2f08dae
Kasutaja <Kasutaja@DESKTOP-O3RE1C7>
2024-11-20 16:01:04 +0200

    Ulesanne 2

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
+#ulesanne 1
 print("Hello World!")
+nimi=input("Mis on sinu nimi? ")
+print("Tere tulemast! Tervitan sind ", nimi)
+print("Tere tulemast! Tervitan sind "+ nimi)
+vanus=int(input("Kui vana sa oled? "))
+print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,"aastat vana")
+print(f"\tTere tulemast! \nTervitan sind {nimi} Sa oled {vanus} aastat vana")
+
+#ulesanne 2
+vanus = 18
+eesnimi = "Jaak"
+pikkus = 16.5
+kas_kaib_koolis = True
+print(type(vanus))
+print(type(pikkus))
+print(type(kas_kaib_koolis))

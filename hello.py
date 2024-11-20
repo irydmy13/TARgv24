134079b9b9da66a09abc87b93a0160334e14c019
Kasutaja <Kasutaja@DESKTOP-O3RE1C7>
2024-11-20 16:35:09 +0200

    ulesanne 4

--- hello.py
+++ hello.py
@@ -1,17 +1,35 @@
-#ulesanne 1
-print("Hello World!")
-nimi=input("Mis on sinu nimi? ")
-print("Tere tulemast! Tervitan sind ", nimi)
-print("Tere tulemast! Tervitan sind "+ nimi)
-vanus=int(input("Kui vana sa oled? "))
-print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,"aastat vana")
-print(f"\tTere tulemast! \nTervitan sind {nimi} Sa oled {vanus} aastat vana")
+from random import * # *-koik funktsioonid
+#import ->
+from math import * #pi ka
+# #ulesanne 1
+# print("Hello World!")
+# nimi=input("Mis on sinu nimi? ") .capitalize() #lower()-aaa, upper()-AAA, capitalize()-Aaa
+# print("Tere tulemast! Tervitan sind ", nimi)
+# print("Tere tulemast! Tervitan sind "+ nimi)
+# vanus=int(input("Kui vana sa oled? "))
+# print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,"aastat vana")
+# print(f"\tTere tulemast! \nTervitan sind {nimi} Sa oled {vanus} aastat vana")
 
-#ulesanne 2
-vanus = 18
-eesnimi = "Jaak"
-pikkus = 16.5
-kas_kaib_koolis = True
-print(type(vanus))
-print(type(pikkus))
-print(type(kas_kaib_koolis))
+# #ulesanne 2
+# vanus = 18
+# eesnimi = "Jaak"
+# pikkus = 16.5
+# kas_kaib_koolis = True
+# print(type(vanus))
+# print(type(eesnimi))
+# print(type(pikkus))
+# print(type(kas_kaib_koolis))
+
+# #ulesanne 3
+# kokku=randint(1,1000)
+# print(f"Kokku on {kokku} kommi")
+# kommi=int(input("Mitu kommi sa tahad? "))
+# kokku=kokku-kommi
+# print(f"Jaak on {kokku} kommi")
+
+#ulesanne 4
+print("Labimoodu leidmine ")
+#l-umbermoot
+l=float(input("Umbermood:" ))
+d=l/pi
+print(f"Labimoodu suurus on {round(d,2)}")
